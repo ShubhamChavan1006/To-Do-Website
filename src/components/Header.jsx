9f4b11c27c36d29f9ed2348e3e353082e383272e
Shubham Chavan <cshubham10603@gmail.com>
2025-05-04 16:30:32 +0530

    To Do Website add code

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+function Header(){
+    return(
+        <header>
+            <div className="logo"> To Do App</div>
+            <div><button className="btn">Login</button></div>
+        </header>
+    );
+}
+
+export default Header;
